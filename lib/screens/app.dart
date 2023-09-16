31592de17a3aff4e1d48be1234ce682f9dbc76c8
esmail.ahmadipour <esmaeil.ahmadipour@samim.net>
2023-09-16 22:35:18 +0330

    refactor(app): adding `InternetStatusWidget` for cover  whole app at above `MaterialApp`

--- lib/screens/app.dart
+++ lib/screens/app.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_connectivity/bloc/bloc.dart';
+import 'package:flutter_bloc_connectivity/widgets/internet_status_widget.dart';
 import 'home_screen.dart';
 
 
@@ -7,12 +10,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter BloC Connectivity',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
+    return BlocProvider(
+      create: (context) => InternetStatusCubit(),
+      child: MaterialApp(
+        builder: (context,widget){
+          return InternetStatusWidget(child: widget!) ;
+        },
+        title: 'Flutter BloC Connectivity',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
+        ),
+        home: const HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
